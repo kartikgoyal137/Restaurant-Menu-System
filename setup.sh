8af22c246f5d09397d82d29f18e06bd4b8b0eafa
Kartik Goyal <kartikcodes137@gmail.com>
2025-06-29 10:47:41 +0530

    modify setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,8 @@
+#!/bin/bash
 echo "Setting up Restaurant Menu System... /kirito"
-
 echo "Installing npm packages..."
 npm install
-
 cp .env.example .env
-
 echo "Importing database..."
 mysql -u root -p < velvetPlate.sql
-
 echo "Add local variables to .env and run [node app.js]"
